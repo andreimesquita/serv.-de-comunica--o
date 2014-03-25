e83b2476cdc120fe0056d1aceefbe189c365e74d
andreirs <andreirs@outlook.com>
2014-03-25 14:19:31 -0300

    #12 - HU1C01 & HU1C02
    alterações em algumas classes e criação de Junit cadastrousuario...
    criação de database test em MySql

--- Sistema/src/com/codigomestre/model/CadastroUsuario.java
+++ Sistema/src/com/codigomestre/model/CadastroUsuario.java
@@ -13,32 +13,36 @@ import com.mysql.jdbc.PreparedStatement;
 public class CadastroUsuario {
 
 	private static final String SQL_TESTE_CADASTRO_EMAIL_EXISTE = "select * from usuarios where email=?";
+	private static final String MENSAGEM_ERRO_EMAIL_EXISTENTE = "Email já cadastrado.";
+	private static final String TABELA_USUARIOS_NOME = "usuarios";
+	
+	
+	
 	private Conexao conexao;
 	
 	
 	public CadastroUsuario () {
-		try {
-			this.conexao = Conexao.getInstance();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		this.conexao = Conexao.getInstance();
 	}
 	
-	public void cadastrar( Usuario u ) throws ErroDuranteCadastroException, SQLException {		
+	public void cadastrar( Usuario u ) throws ErroDuranteCadastroException, ErroDeConexaoException, SQLException {		
 		
 		PreparedStatement objprep = conexao.getRetornarEstadoDePreparo(SQL_TESTE_CADASTRO_EMAIL_EXISTE); 
 		objprep.setString(1, u.getEmail());
 		ResultSet objrs = objprep.executeQuery();
-		objrs.get
-		
-		if (u != null) {
-			throw new ErroDuranteCadastroException("Email já cadastrado.");
+		boolean b = false;
+		while ( objrs.next() ) {
+			b=true;
 		}
+		if ( b ) {
+			throw new ErroDuranteCadastroException(MENSAGEM_ERRO_EMAIL_EXISTENTE);
+		}
+		
 	}
 	
-	public static void reset() {
-		lista.clear();
+	public void reset() throws SQLException, ErroDeConexaoException {
+		PreparedStatement objprep = conexao.getRetornarEstadoDePreparo( "truncate table ".concat(TABELA_USUARIOS_NOME)); 
+		objprep.execute();
 	}
 	
 }
