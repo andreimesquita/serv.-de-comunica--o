454f15b7e3954302d9aba45d6efb4e9f092fbeea
Maurício Carvalho <mauricio_carvalho51@yahoo.com.br>
2014-04-10 11:25:31 -0300

    teste conexão

--- Sistema/src/com/codigomestre/servidor/Main.java
+++ Sistema/src/com/codigomestre/servidor/Main.java
@@ -1,5 +1,10 @@
 package com.codigomestre.servidor;
 
+/**
+ * 
+ * @author mauricio.victor.carvalho@gmail.com
+ *
+ */
 public class Main {
 	public static void main(String[] args) throws Exception {
 		new Servidor();
